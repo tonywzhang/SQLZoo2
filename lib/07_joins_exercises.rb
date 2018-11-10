da95e6ff0efc96275b4fa88943bad10fff6ea099
Tony <tonywzhang@gmail.com>
2018-11-10 14:13:52 -0800

    07 Joins Exercises 90% Completed

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -121,7 +121,18 @@ def andrews_films_and_leads
   JOIN
     actors ON castings.actor_id = actors.id
   WHERE
-    actors.name = 'Julie Andrews'
+    castings.ord = 1 AND movies.id IN (
+      SELECT
+        movies.id
+      FROM
+        movies
+      JOIN
+        castings on movies.id = castings.movie_id
+      JOIN
+        actors ON castings.actor_id = actors.id
+      WHERE
+        actors.name = 'Julie Andrews'
+    )
   SQL
 end
 
@@ -129,6 +140,20 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    actors
+  JOIN
+    castings on actors.id = castings.actor_id
+  WHERE
+    castings.ord = 1
+  GROUP BY
+    name
+  HAVING
+    COUNT(*) >= 15
+  ORDER BY
+    name
   SQL
 end
 
@@ -136,6 +161,20 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      movies.title, COUNT(*)
+    FROM
+      movies
+    JOIN
+      castings on movies.id = castings.movie_id
+    JOIN
+      actors on actors.id = castings.actor_id
+    WHERE
+      yr = 1978
+    GROUP BY
+      movies.title
+    ORDER BY
+      COUNT(*) DESC, movies.title ASC
   SQL
 end
 
